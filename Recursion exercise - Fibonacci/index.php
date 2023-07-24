558969ef001ab743c8b905a9249c37ff73d01027
Paulo Emmanuel <pauloemmanuelferreira@gmail.com>
2023-07-23 21:22:17 -0300

    fix: fixing initial sequence

--- Recursion exercise - Fibonacci/index.php
+++ Recursion exercise - Fibonacci/index.php
@@ -2,14 +2,14 @@
 
 
 
-$n = 5; 
+$n = 4; 
 $result = fibonacci($n);
 echo "The {$n}th number in the Fibonacci sequence is: {$result}";
 function fibonacci($n)
 {
-    if ($n == 0) { 
+    if ($n == 1 || $n == 0) { 
         return 0;
-    } elseif ($n == 1) { 
+    } elseif ($n == 2) { 
         return 1;
     } else {
         return fibonacci($n - 1) + fibonacci($n - 2);
